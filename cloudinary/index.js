8056b0f1b94742dd9847e5ce3739ed7c6d481b9b
ahmedelselly <ahmedelselly@localhost.localdomain>
2022-05-19 14:23:00 +0200

    added Arun's cloudinary account credentials

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,19 +2,19 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const crypto = require('crypto');
 
+// cloudinary.config({
+// 	cloud_name: 'elselly',
+// 	api_key: '549562546544323',
+// 	api_secret: 'blNBwf7gQVPvt8LdgSN02BbSYnc',
+// 	secure: true
+// });
+
 cloudinary.config({
-	cloud_name: 'elselly',
-	api_key: '549562546544323',
-	api_secret: 'blNBwf7gQVPvt8LdgSN02BbSYnc',
+	cloud_name: 'dsa0uaq9r',
+	api_key: '845686327439385',
+	api_secret: 'EwMsQ66hH3P3jbV-2g63fyl9coo',
 	secure: true
 });
-
-// cloudinary.config({
-// 	cloud_name: 'dsa0uaq9r',
-// 	api_key: '845686327439385',
-// 	api_secret: 'EwMsQ66hH3P3jbV-2g63fyl9coo'
-	// secure: true
-// 	});
 
 const storage = new CloudinaryStorage({
 	cloudinary: cloudinary,
